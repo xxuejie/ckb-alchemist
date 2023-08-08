0aed41df0e4fa51066e2931a4a9cf616f972d96e
Xuejie Xiao <xxuejie@gmail.com>
2023-08-08 07:22:22 +0000

    Add CKB hasher

--- src/app.rs
+++ src/app.rs
@@ -13,6 +13,8 @@ fn blake2b_256<T: AsRef<[u8]>>(s: T) -> [u8; 32] {
 pub struct RootApp {
     pub script_hash_window: ScriptHash,
     pub script_hash_open: bool,
+    pub ckb_hash_window: CkbHash,
+    pub ckb_hash_open: bool,
 }
 
 impl Default for RootApp {
@@ -20,6 +22,8 @@ impl Default for RootApp {
         Self {
             script_hash_window: ScriptHash::default(),
             script_hash_open: true,
+            ckb_hash_window: CkbHash::default(),
+            ckb_hash_open: true,
         }
     }
 }
@@ -41,6 +45,7 @@ impl eframe::App for RootApp {
 
             egui::containers::ScrollArea::vertical().show(ui, |ui| {
                 ui.toggle_value(&mut self.script_hash_open, self.script_hash_window.name());
+                ui.toggle_value(&mut self.ckb_hash_open, self.ckb_hash_window.name());
             });
 
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
@@ -69,6 +74,7 @@ impl eframe::App for RootApp {
 
         self.script_hash_window
             .draw(ctx, &mut self.script_hash_open);
+        self.ckb_hash_window.draw(ctx, &mut self.script_hash_open);
     }
 }
 
@@ -187,6 +193,56 @@ impl ScriptHash {
 
         if let Err(e) = result {
             ui.label(egui::RichText::new(e).color(egui::Color32::RED));
+        }
+    }
+}
+
+#[derive(Default)]
+pub struct CkbHash {
+    pub content: String,
+}
+
+impl CkbHash {
+    pub fn name(&self) -> String {
+        "CKB Hasher".to_string()
+    }
+
+    pub fn draw(&mut self, ctx: &egui::Context, open: &mut bool) {
+        egui::Window::new(self.name())
+            .open(open)
+            .show(ctx, |ui| self.ui(ui));
+    }
+
+    fn ui(&mut self, ui: &mut egui::Ui) {
+        ui.label("Content in HEX:");
+        ui.text_edit_multiline(&mut self.content);
+
+        let raw_data = {
+            let mut value = self.content.as_str();
+            if value.starts_with("0x") {
+                value = &value[2..];
+            }
+            match hex::decode(value) {
+                Ok(data) => Ok(data),
+                Err(e) => Err(format!("Error parsing hex content: {}", e)),
+            }
+        };
+
+        match raw_data {
+            Ok(data) => {
+                let script_hash = format!("0x{:x}", Bytes::from(blake2b_256(&data).to_vec()));
+
+                ui.horizontal(|ui| {
+                    ui.label("Script Hash: ");
+                    if ui.button("📋").on_hover_text("Click to copy").clicked() {
+                        ui.output_mut(|o| o.copied_text = script_hash.clone());
+                    }
+                });
+                ui.add(egui::Label::new(script_hash).wrap(true));
+            }
+            Err(e) => {
+                ui.label(egui::RichText::new(e).color(egui::Color32::RED));
+            }
         }
     }
 }
